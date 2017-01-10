d9b17901c016eb4f12962126df7367502ddf41a9
Carlos Pava <carlos.pava@sundevs.com>
2017-01-09 20:32:20 -0800

    Initial Commit

---
+++ PruebaFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaFirebase
+//
+//  Created by Carlos Pava on 9/01/17.
+//  Copyright © 2017 Sundevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
